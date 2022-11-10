e2b4bdcbed1e846cf8738c6aaf8395b623629999
Vane737 <77525733+Vane737@users.noreply.github.com>
2022-11-10 16:44:26 -0600

    Gestionar_Informe Añadido

---
+++ src/negocio/Gestionar_Informe.java
@@ -0,0 +1,170 @@
+package negocio;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import datos.conexion;
+import funciones.cadenas;
+
+public class Gestionar_Informe {
+
+    int id;
+    String nombre;
+
+    conexion m_Conexion;
+    cadenas cade = new cadenas();
+
+    public Gestionar_Informe() {
+        this.m_Conexion = conexion.getInstancia();
+    }
+
+    public void setIdGrupo(int id) {
+        this.id = id;
+    }
+
+    public void AsigParametros(String patron) {
+        String vector[];
+        patron = patron.replace('"', ' ');
+        patron = patron.replace('[', ' ');
+        patron = patron.replace(']', ' ');
+        patron = patron.trim();
+        vector = patron.split(",");
+        this.id = Integer.parseInt(vector[0].trim());
+        this.nombre = vector[1].trim();
+
+    }
+
+    public int InsertarGrupo() {
+
+        // Abro y obtengo la conexion
+        this.m_Conexion.abrirConexion();
+        Connection con = this.m_Conexion.getConexion();
+        int i = 1;
+
+        // Preparo la consulta
+        String sql = "INSERT INTO grupos(\n"
+                + "	id, nombre)\n"
+                + "	VALUES (?, ?)";
+
+        try {
+            // La ejecuto
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            // El segundo parametro de usa cuando se tienen tablas que generan llaves primarias
+            // es bueno cuando nuestra bd tiene las primarias aut	oincrementables
+            ps.setInt(1, this.id);
+            ps.setString(2, this.nombre);
+
+            int rows = ps.executeUpdate();
+
+            // Cierro Conexion
+            this.m_Conexion.cerrarConexion();
+
+            // Obtengo el id generado pra devolverlo
+            if (rows != 0) {
+                ResultSet generateKeys = ps.getGeneratedKeys();
+                if (generateKeys.next()) {
+                    return generateKeys.getInt(1);
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            i = -1;
+        }
+        return i;
+    }
+
+    public String ListarGrupo() {
+        String res = "";
+
+        // Abro y obtengo la conexion
+        this.m_Conexion.abrirConexion();
+        Connection con = this.m_Conexion.getConexion();
+        // Preparo la consulta
+        String sql = "SELECT * \n"
+                + "FROM grupos";
+
+        try {
+            // La ejecuto
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            // Cierro la conexion
+            this.m_Conexion.cerrarConexion();
+            res = "<table border=\"6\"><caption><b>Lista de Servicio</b></caption>\n"
+                    + "<tr><th style=background:#8fe5f6;>ID</th><th style=background:#8fe5f6;>Nombre del Grupo</th>";
+            // Recorro el resultado
+            while (rs.next()) {
+                res = res + "<tr><td>" + rs.getInt("id") + "</td><td>" + rs.getString("nombre") + "</td></tr>";
+            }
+            res = res + "</table>";
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return res;
+    }
+
+    public void AsigParametrosMod(String patron) {
+        String vector[];
+        patron = patron.replace('"', ' ');
+        patron = patron.replace('[', ' ');
+        patron = patron.replace(']', ' ');
+        patron = patron.trim();
+        vector = patron.split(",");
+        this.id = Integer.parseInt(vector[0].trim());
+        this.nombre = vector[1].trim();
+
+    }
+
+    public int ModifGrupo() {
+        int c = 1;
+        // Abro y obtengo la conexion
+        this.m_Conexion.abrirConexion();
+        Connection con = this.m_Conexion.getConexion();
+
+        // Preparo la consulta
+        String sql = "UPDATE grupos SET \n"
+                + "nombre = ? \n"
+                + "WHERE id = ?";
+        try {
+            // La ejecuto
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, this.nombre);
+            ps.setInt(2, this.id);
+            int rows = ps.executeUpdate();
+            // Cierro la conexion
+            this.m_Conexion.cerrarConexion();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            c = -1;
+        }
+        return c;
+
+    }
+
+    public int EliminarGrupo() {
+        int i = 1;
+        // Abro y obtengo la conexion
+        this.m_Conexion.abrirConexion();
+        Connection con = this.m_Conexion.getConexion();
+
+        // Preparo la consulta
+        String sql = "DELETE from grupos \n"
+                + "WHERE id = ?";
+        try {
+            // La ejecuto
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, this.id);
+            int rows = ps.executeUpdate();
+
+            // Cierro la conexion
+            this.m_Conexion.cerrarConexion();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            i = -1;
+        }
+        return i;
+    }
+
+}
